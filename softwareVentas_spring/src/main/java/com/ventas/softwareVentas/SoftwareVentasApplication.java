e915437fe724d7381c842cadc9b23b3e48de46da
Miguel Angel <oquendorinconmiguelangel@gmail.com>
2021-11-23 10:24:19 -0500

    Se agrega proyecto Spring

---
+++ softwareVentas_spring/src/main/java/com/ventas/softwareVentas/SoftwareVentasApplication.java
@@ -0,0 +1,13 @@
+package com.ventas.softwareVentas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareVentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareVentasApplication.class, args);
+	}
+
+}
---
+++ softwareVentas_spring/src/test/java/com/ventas/softwareVentas/SoftwareVentasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ventas.softwareVentas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareVentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
